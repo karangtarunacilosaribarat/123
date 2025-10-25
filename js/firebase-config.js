2b4964c0e465ac03301c56e9b97e440955042c75
karangtarunacilosaribarat <karangtarunacilosaribarat@gmail.com>
2025-10-25 16:37:48 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,19 @@
-// Import SDK Firebase modular
+// Firebase Modular SDK v10+ (CDN)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-// Konfigurasi Firebase KARTEJI
 const firebaseConfig = {
   apiKey: "AIzaSyAjNVTi5ZjZDLRcfxXmf2gWmHswSHM4d8E",
   authDomain: "karteji.firebaseapp.com",
   projectId: "karteji",
-  storageBucket: "karteji.appspot.com", // ✅ perbaikan di sini
+  storageBucket: "karteji.appspot.com",
   messagingSenderId: "828706251907",
   appId: "1:828706251907:web:54825185b074209c4fe7b6",
   measurementId: "G-PPGRMBXGHW"
 };
 
-// Inisialisasi Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
