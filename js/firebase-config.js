ee9dc220bd4f76a4eed54a290228fee5d8304a48
karangtarunacilosaribarat <karangtarunacilosaribarat@gmail.com>
2025-10-25 16:01:49 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,19 +1,21 @@
-
-// Replace with your Firebase config (from your project settings)
+// Import SDK Firebase modular
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+// Konfigurasi Firebase KARTEJI
+const firebaseConfig = {
+  apiKey: "AIzaSyAjNVTi5ZjZDLRcfxXmf2gWmHswSHM4d8E",
+  authDomain: "karteji.firebaseapp.com",
+  projectId: "karteji",
+  storageBucket: "karteji.appspot.com", // ✅ perbaikan di sini
+  messagingSenderId: "828706251907",
+  appId: "1:828706251907:web:54825185b074209c4fe7b6",
+  measurementId: "G-PPGRMBXGHW"
 };
 
+// Inisialisasi Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
